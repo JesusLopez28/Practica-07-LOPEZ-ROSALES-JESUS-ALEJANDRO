e01d22598803fc4cc52d02d04f08dd4accf25f9d
JesusLopez28 <jesus.lopez280402@gmail.com>
2023-04-21 12:54:42 -0600

    07

---
+++ src/practica/pkg07/lopez/rosales/jesus/alejandro/Practica07LOPEZROSALESJESUSALEJANDRO.java
@@ -0,0 +1,14 @@
+/*
+López Rosales Jesús Alejandro
+22110104       3°O        POE
+*/
+
+package practica.pkg07.lopez.rosales.jesus.alejandro;
+
+public class Practica07LOPEZROSALESJESUSALEJANDRO {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
